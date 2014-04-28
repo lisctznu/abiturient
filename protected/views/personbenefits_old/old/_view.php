8ee5dbe5af0c553446ced55f6bd0eac21a984821
IT.ZNU.EDU <sinepolskiyihor@gmail.com>
2014-04-28 08:49:49 +0300

    fixed PersonSexTypes, deleted unusable files

--- protected/views/personbenefits_old/old/_view.php
+++
@@ -1,6 +0,0 @@
-<?php $form = new CActiveForm(); ?>
-<?php foreach($models as $model): ?>
-<div class="row-fluid">
-     <?php echo $form->dropDownList($model, "[]BenefitID", Benefit::DropDown(), array('class'=>"span12"));?>
-</div>
-<?php endforeach; ?>
